22c4abdbf8d23744f8e924ac8768b3b7e8744474
Rafael Soriano Ramírez <rsoriano@spsolutions.com.mx>
2023-05-04 09:48:03 -0600

    [Add], paths

--- core_mf_full/mf_users/src/bootstrap.js
+++ core_mf_full/mf_users/src/bootstrap.js
@@ -27,7 +27,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
 }
 
 if (process.env.NODE_ENV === 'development') {
-	const devRoot = document.querySelector('#root')
+	const devRoot = document.querySelector('#_users-dev-root')
 
 	if (devRoot) {
 		mount(devRoot, { defaultHistory: createBrowserHistory() })
